07633d2971a884dcac472a8677f8cd53393edfb9
eeyoo <feilinwu2036@gmail.com>
2017-08-08 23:36:06 +0800

    add shipit file

---
+++ shipitfile.js
@@ -0,0 +1,26 @@
+/**
+ * Created by feilin on 2017/8/8.
+ */
+module.exports = function (shipit) {
+    require('shipit-deploy')(shipit);
+
+    shipit.initConfig({
+        default: {
+            workspace: '/tmp/dev',
+            deployTo: '/tmp/deploy',
+            repositoryUrl: 'https://github.com/eeyoo/express-bootstrap.git',
+            ignores: ['.git', 'node_modules'],
+            rsync: ['--del'],
+            keepReleases: 2,
+            key: '',
+            shallowClone: true
+        },
+        staging: {
+            servers: '192.168.3.39'
+        }
+    });
+
+    shipit.task('pwd', function () {
+        return shipit.remote('pwd');
+    });
+};
